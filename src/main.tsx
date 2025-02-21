18eead17a7106ffcecc764c24a41b0abd837b355
Shamil Kaleel <155573196+ShamilKaleel@users.noreply.github.com>
2025-02-21 20:10:14 +0530

    import as @/context/authContext.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import { AuthProvider } from "@/contexts/authContext.tsx";
 
-import { AuthProvider } from "./contexts/authContext";
 import { BookingProvider } from "./contexts/bookingContext.tsx";
 import { DentistProvider } from "./contexts/dentistContext.tsx";
 import { ScheduleProvider } from "./contexts/scheduleContext.tsx";
